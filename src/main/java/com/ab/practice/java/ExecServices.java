c9826b829c9ad1ec24846ad43721ee7cdfdccec7
Ajit Bhandari <bhandari.ajit786@gmail.com>
2025-03-11 20:03:46 +0530

    exec services frameowrk

---
+++ src/main/java/com/ab/practice/java/ExecServices.java
@@ -0,0 +1,14 @@
+package com.ab.practice.java;
+
+import java.util.concurrent.ExecutorService;
+
+public class ExecServices {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stube
+		ExecutorService e;
+		//e.execute(null);
+
+	}
+
+}
